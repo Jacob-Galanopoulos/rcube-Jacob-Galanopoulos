e5ad3b606ede1fa68272515ce7338458dab27f00
Jacob Galanopoulos <jakegalanopoulos@yahoo.com>
2020-10-22 18:24:24 -0500

    Yellow Light |

--- RCube/check.py
+++ RCube/check.py
@@ -5,12 +5,6 @@ def _check(parms):
     if(parms['cube'] == ''): return {'status': 'error: missing cube value'}
     if(len(parms['cube']) != 54): return {'status': 'error: cube does not have correct number of elements'}
     
-    if(not('integrity' in parms)): return {'status': 'error: missing integrity value'}
-    if(parms['integrity'] == ''): return {'status': 'error: missing integrity key'}
-    integrity = bytes(parms['cube'], 'utf-8')
-    if(not(parms['integrity'] == hashlib.sha256(integrity).hexdigest().upper())):
-        return {'status': 'error: incorrect integrity value'}
-    
     cubeFaces = [parms['cube'][i:i+9] for i in range(0, len(parms['cube']), 9)]
     cubeCenterColors = []
     cubeColors = []
@@ -30,6 +24,12 @@ def _check(parms):
     
     #if(not(edge['result'])): {'status': 'error: cube has an edge that can not exist'}
     #if(not(corner['result'])): {'status': 'error: cube has a corner that can not exist'}
+    
+    if(not('integrity' in parms)): return {'status': 'error: missing integrity value'}
+    if(parms['integrity'] == ''): return {'status': 'error: missing integrity key'}
+    integrity = bytes(parms['cube'], 'utf-8')
+    if(not(parms['integrity'] == hashlib.sha256(integrity).hexdigest().upper())):
+        return {'status': 'error: incorrect integrity value'}
     
     #return _status(cubeFaces, cubeCenterColors)
 
