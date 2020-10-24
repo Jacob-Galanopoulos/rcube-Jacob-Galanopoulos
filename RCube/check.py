0d4e883167d24164b8169f518e9c636d6c0bb44e
Jacob Galanopoulos <jakegalanopoulos@yahoo.com>
2020-10-24 11:08:21 -0500

    Green Light |

--- RCube/check.py
+++ RCube/check.py
@@ -66,13 +66,18 @@ def _validateEdges(cubeFaces = [], cubeCenterColors = []):
     if(len(cubeFaces) == 0): return {'error': 'missing input'}
     if(len(cubeCenterColors) == 0): return {'error': 'missing input'}
     
+    #To check the edge statically, we have to map all of the cube.
+    #This requires us to choose points on the cube based off which face
+    #(in order of front, right, back, left, top and bottom) and to choose
+    #the edge piece based off it's space on the face. I hate magic numbers 
+    #but this is the price I pay
     isValidEdgeCube = True
     edgeMap =[cubeFaces[0][1] + cubeFaces[4][7], cubeFaces[0][3] + cubeFaces[3][5],
-              cubeFaces[0][5] + cubeFaces[1][3], cubeFaces[0][7] + cubeFaces[5][1],
-              cubeFaces[2][1] + cubeFaces[4][1], cubeFaces[2][3] + cubeFaces[1][5],
-              cubeFaces[2][5] + cubeFaces[3][3], cubeFaces[2][7] + cubeFaces[5][7],
-              cubeFaces[1][1] + cubeFaces[4][3], cubeFaces[1][7] + cubeFaces[5][5],
-              cubeFaces[3][1] + cubeFaces[4][3], cubeFaces[3][7] + cubeFaces[5][3]]
+        cubeFaces[0][5] + cubeFaces[1][3], cubeFaces[0][7] + cubeFaces[5][1],
+        cubeFaces[2][1] + cubeFaces[4][1], cubeFaces[2][3] + cubeFaces[1][5],
+        cubeFaces[2][5] + cubeFaces[3][3], cubeFaces[2][7] + cubeFaces[5][7],
+        cubeFaces[1][1] + cubeFaces[4][3], cubeFaces[1][7] + cubeFaces[5][5],
+        cubeFaces[3][1] + cubeFaces[4][3], cubeFaces[3][7] + cubeFaces[5][3]]
     
     for edge in edgeMap:
         if ((cubeCenterColors[0] in edge and cubeCenterColors[2] in edge) or
@@ -86,15 +91,20 @@ def _validateCorners(cubeFaces = [], cubeCenterColors = []):
     if(len(cubeFaces) == 0): return {'error': 'missing input'}
     if(len(cubeCenterColors) == 0): return {'error': 'missing input'}
     
+    #To check the corner statically, we have to map all of the cube.
+    #This requires us to choose points on the cube based off which face
+    #(in order of front, right, back, left, top and bottom) and to choose
+    #the corner piece based off it's space on the face. Kinda the same as edge
+    #but different indexes
     isValidCornerCube = True
     cornerMap = [cubeFaces[0][0] + cubeFaces[3][2] + cubeFaces[4][6], 
-                  cubeFaces[0][2] + cubeFaces[1][0] + cubeFaces[4][8], 
-                  cubeFaces[0][6] + cubeFaces[3][8] + cubeFaces[5][0], 
-                  cubeFaces[0][8] + cubeFaces[1][6] + cubeFaces[5][2], 
-                  cubeFaces[2][0] + cubeFaces[1][3] + cubeFaces[4][2], 
-                  cubeFaces[2][2] + cubeFaces[4][0] + cubeFaces[3][2], 
-                  cubeFaces[2][6] + cubeFaces[1][8] + cubeFaces[5][2], 
-                  cubeFaces[2][8] + cubeFaces[3][6] + cubeFaces[5][8]]
+        cubeFaces[0][2] + cubeFaces[1][0] + cubeFaces[4][8], 
+        cubeFaces[0][6] + cubeFaces[3][8] + cubeFaces[5][0], 
+        cubeFaces[0][8] + cubeFaces[1][6] + cubeFaces[5][2], 
+        cubeFaces[2][0] + cubeFaces[1][3] + cubeFaces[4][2], 
+        cubeFaces[2][2] + cubeFaces[4][0] + cubeFaces[3][2], 
+        cubeFaces[2][6] + cubeFaces[1][8] + cubeFaces[5][2], 
+        cubeFaces[2][8] + cubeFaces[3][6] + cubeFaces[5][8]]
     
     for corner in cornerMap:
         if ((cubeCenterColors[0] in corner and cubeCenterColors[2] in corner) or
