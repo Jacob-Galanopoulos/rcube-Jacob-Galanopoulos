883723fd2c8ffe8a6caeaa2a6f764169a20ae175
Jacob Galanopoulos <jakegalanopoulos@yahoo.com>
2020-11-17 17:21:13 -0600

    Red Light |

--- RCube/rotate.py
+++ RCube/rotate.py
@@ -2,14 +2,14 @@ def _rotate(parms):
     result = {'rotate': 'rotate stub'}
     return result
 
-def _execFrontAndBack(parms):
+def _execFrontAndBack(cubeFaces, rotateDirection):
     result = {'rotate': 'rotate stub'}
     return result
 
-def _execLeftAndRight(parms):
+def _execLeftAndRight(cubeFaces, rotateDirection):
     result = {'rotate': 'rotate stub'}
     return result
 
-def _execTopAndBottom(parms):
+def _execTopAndBottom(cubeFaces, rotateDirection):
     result = {'rotate': 'rotate stub'}
     return result
